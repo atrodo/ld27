96d9f10e472180f819fe00794525d7b7953df3f7
Jon Gentle <atrodo@atrodo.org>
2013-08-25 15:09:12 -0400

    Round out the tutorial puzzles

--- ld27_js/puzzles.js
+++ ld27_js/puzzles.js
@@ -29,6 +29,25 @@ var puzzles = [
   },
   {
     actions: [
+      new Action(2),
+      new Action(6),
+      new Action(2, {pos: {x: 4, y: 0}, fixed: true}),
+    ],
+    rows: 1,
+  },
+  {
+    actions: [
+      new Action(2),
+      new Action(2),
+      new Action(8),
+      new Action(2, {pos: {x: 4, y: 0}, show: false}),
+    ],
+    rows: 2,
+  },
+
+  // End Tutorial
+  {
+    actions: [
       new Action(2.5),
       new Action(2.5),
       new Action(5),
@@ -36,5 +55,15 @@ var puzzles = [
       new Action(4),
     ],
     rows: 2,
+  },
+  {
+    actions: [
+      new Action(3.5),
+      new Action(2.5),
+      new Action(2),
+      new Action(6),
+      new Action(1),
+    ],
+    rows: 3,
   },
 ]
